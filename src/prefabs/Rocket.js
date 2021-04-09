42f5186793a5396f750fe2c7fa2048ddf712bbd5
Brandon <bimcdona@ucsc.edu>
2021-04-09 16:01:24 -0700

    Issue fixed?

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,7 +1,7 @@
 // Player Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture, frame){
-        super(scene, x, y, frame);
+        super(scene, x, y, texture, frame);
 
         // add to scene
         scene.add.existing(this);
